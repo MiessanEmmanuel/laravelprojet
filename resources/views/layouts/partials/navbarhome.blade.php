3cf3b682e54cc4096a91619d3d3db54686f97f0a
MiessanEmmanuel <emmanueljeanmiessan@gmail.com>
2022-11-24 18:31:45 +0100

    menu

--- resources/views/layouts/partials/navbarhome.blade.php
+++ resources/views/layouts/partials/navbarhome.blade.php
@@ -7,7 +7,7 @@
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
-      <nav id="navbar" class="navbar">
+      <nav id="navbar" class="navbar ">
         <ul>
           <li><a class="nav-link scrollto active" href="/">Home</a></li>
           <li><a class="nav-link scrollto" href="about">About</a></li>
